3221d8e3d0b08cfedd0c70b1671dbac13382d33b
woon rwee young <woonrweeyoung@gmail.com>
2024-04-27 13:03:17 +0800

    side: last line removed
    side: last line removed

--- random.py
+++ random.py
@@ -3,5 +3,4 @@ import random
 random_numbers = [random.randint(1, 100) for _ in range(10)]
 print("Random numbers:", random_numbers)
 
-sorted_numbers = sorted(random_numbers)
-print("Sorted numbers:", sorted_numbers)
+# Sort the list of random numbers
